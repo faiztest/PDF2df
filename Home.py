a4ea6afffa0c7b2b6cfdabe3e1364c92782c7b13
Faizhal A. S <126218330+faizhalas@users.noreply.github.com>
2023-06-22 08:43:27 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,7 +25,7 @@ def split(extracted_data):
     df = pd.DataFrame(extracted_data)
     df = df.replace(r'\n',' ', regex=True) 
     pattern = '|'.join(word_list)
-    split_df = df['Values'].str.split(pattern, expand=True)
+    split_df = df['Text'].str.split(pattern, expand=True)
     result_df = pd.concat([rd, split_df], axis=1)
     new_columns = ['File Name', 'Text', 'intro'] + word_list
     result_df.columns = new_columns
@@ -38,7 +38,6 @@ st.header("Upload PDF Files")
 uploaded_files = st.file_uploader("Choose files", type=['pdf'], accept_multiple_files=True)
 text_search = st.text_input("Split your PDFs into parts. Separate words (cAsE sEnSiTiVe) by semicolons (;)")
 word_list = [keyword.strip() for keyword in text_search.split(";")]
-st.write(word_list)
 
 if st.button("Convert"):
     extracted_data = convert(uploaded_files)
